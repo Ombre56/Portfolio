391ff2f24275d02a79e313fddeeeb00d19e54758
Ombre <krys1014@wp.pl>
2022-10-07 20:38:36 +0200

    Change main and add first section ,,Home''

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,22 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import HomePage from '../sections/HomePage'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>My Portfolio</title>
         <meta name="description" content="Portfolio Krystian Heluszka" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to on my <a>Portfolio</a>
-        </h1>
-
-        <p className={styles.description}>
-          Website is under construction
-        </p>
-
-        <Image src="/images/image.svg" width={500} height={500} alt="Image"/>
-
+      <main>
+        <HomePage />
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer>
           Copyright © by Ombre
-      </footer>
-    </div>
+      </footer> */}
+    </>
   )
 }
