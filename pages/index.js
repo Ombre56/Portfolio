6e565ed49a094e49695f15663879f0c1c2d74daa
Ombre <krys1014@wp.pl>
2022-12-30 18:08:44 +0100

    Chenged first page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,10 @@
-import Head from 'next/head'
+import Layout from '../components/Layout';
 import HomePage from '../sections/HomePage'
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>My Portfolio</title>
-        <meta name="description" content="Portfolio Krystian Heluszka" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <HomePage />
-      </main>
-
-    </>
+    <Layout>
+      <HomePage />
+    </Layout>
   )
 }
