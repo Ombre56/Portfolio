cf25541b3c2380e8401c77c12220a5b74260c347
Ombre <krys1014@wp.pl>
2022-12-30 18:09:18 +0100

    Added new page

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import AboutPage from '../sections/AboutPage';
+
+export default function Home() {
+  return (
+    <Layout>
+      <AboutPage />
+    </Layout>
+  )
+}
