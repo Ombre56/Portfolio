fe93bd289886fe8cf927c30deab0dd7d1165efad
Ombre <krys1014@wp.pl>
2023-01-03 20:38:31 +0100

    Rename name of function

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Layout from '../components/Layout';
 import AboutPage from '../sections/AboutPage';
 
-export default function Home() {
+export default function About() {
   return (
     <Layout>
       <AboutPage />
